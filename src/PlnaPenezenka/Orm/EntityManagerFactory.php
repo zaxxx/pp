80dd6b4746444212b7b72bb85455d1a61e12b38b
Lukáš Klika <info@lukasklika.cz>
2019-10-16 17:31:55 +0200

    Setup Doctrine ORM

---
+++ src/PlnaPenezenka/Orm/EntityManagerFactory.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+namespace PlnaPenezenka\Orm;
+
+use Doctrine\ORM\Configuration;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\ORMException;
+use Doctrine\ORM\Tools\Setup;
+use PDO;
+
+class EntityManagerFactory
+{
+    /** @var string[] */
+    private $paths;
+
+    /** @var string */
+    private $proxyDir;
+
+    /** @var bool */
+    private $devMode;
+
+    /** @var PDO */
+    private $pdo;
+
+    public function __construct(array $paths, string $proxyDir, bool $devMode, PDO $pdo)
+    {
+        $this->paths = $paths;
+        $this->proxyDir = $proxyDir;
+        $this->devMode = $devMode;
+        $this->pdo = $pdo;
+    }
+
+    /**
+     * @return EntityManagerInterface
+     * @throws ORMException
+     */
+    public function create(): EntityManagerInterface
+    {
+        return EntityManager::create([
+            'driver' => 'pdo_mysql',
+            'pdo' => $this->pdo,
+        ], $this->createConfiguration());
+    }
+
+    private function createConfiguration(): Configuration
+    {
+        return Setup::createAnnotationMetadataConfiguration($this->paths, $this->devMode, $this->proxyDir);
+    }
+}
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,24 +1,29 @@
 <?php
 declare(strict_types=1);
 
+use Nette\Application\Application;
 use Nette\DI\Compiler;
-use Nette\DI\Config\Loader;
 use Nette\DI\Container;
 use Nette\DI\ContainerLoader;
+use Symfony\Component\Console\Application as ConsoleApplication;
 use Tracy\Debugger;
 
-$configFile = __DIR__ . '/config/config.neon';
-$configLoader = new Loader();
-$config = $configLoader->load($configFile, false);
-
-$devMode = $config['devMode'] ?? false;
+$devMode = file_exists(__DIR__ . '/config/dev.neon');
 
 Debugger::enable(!$devMode, __DIR__ . '/../temp/log');
+Debugger::$strictMode = true;
 
 $containerLoader = new ContainerLoader(__DIR__ . '/../temp/cache', $devMode);
-$class = $containerLoader->load(function (Compiler $compiler) use ($configFile, $configLoader) {
-    $compiler->loadConfig($configFile, $configLoader);
+$class = $containerLoader->load(function (Compiler $compiler) use ($devMode) {
+    $compiler->loadConfig(__DIR__ . '/config/config.neon');
+    $compiler->addConfig(['parameters' => ['devMode' => $devMode]]);
 }, 'container');
 
 /** @var Container $container */
 $container = new $class();
+
+if (php_sapi_name() === 'cli') {
+    $container->getByType(ConsoleApplication::class)->run();
+} else {
+    $container->getByType(Application::class)->run();
+}
